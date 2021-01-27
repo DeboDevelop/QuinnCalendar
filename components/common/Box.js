fcb75017bad463b92a30be395ef9a35543629174
DeboDevelop <debajyotidofficial@gmail.com>
2021-01-27 14:50:46 +0530

    Created the initial grid for calendar and a header.

---
+++ components/common/Box.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Dimensions, StyleSheet, Text, TouchableOpacity } from "react-native";
+
+const { width: MAX_WIDTH, height: MAX_HEIGHT } = Dimensions.get("window");
+
+const Box = () => {
+    return (
+        <TouchableOpacity style={styles.box}>
+            <Text>0</Text>
+        </TouchableOpacity>
+    );
+};
+
+const styles = StyleSheet.create({
+    box: {
+        height: (MAX_HEIGHT - 120) / 7,
+        width: MAX_WIDTH / 7,
+        backgroundColor: "#f0e1af",
+        borderWidth: 1,
+        borderColor: "#000000",
+    },
+});
+
+export default Box;
--- components/pages/Calendar.js
+++ components/pages/Calendar.js
@@ -1,21 +1,64 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export class Calendar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            calender: Array(49).fill(0),
+            day: new Date().getDate(),
+            month: new Date().getMonth(),
+            year: new Date().getFullYear(),
+        };
+    }
     render() {
         return (
-            <View style={styles.main}>
-                <Text> Hello World!!! </Text>
+            <View>
+                <View style={styles.header}>
+                    <TouchableOpacity>
+                        <Text style={styles.txt}>December</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.btn}>
+                        <Text style={styles.txt}>Today</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.main}>
+                    {/* {this.state.calender.map((item, index) => {
+                        return <Box key={index} />;
+                    })} */}
+                    <Text>{this.state.day}</Text>
+                    <Text>{this.state.month}</Text>
+                    <Text>{this.state.year}</Text>
+                </View>
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
+    header: {
+        backgroundColor: "#4287f5",
+        flexDirection: "row",
+        flexWrap: "nowrap",
+        justifyContent: "space-between",
+        height: 40,
+        paddingTop: 5,
+        paddingBottom: 5,
+    },
+    txt: {
+        marginLeft: 5,
+        marginRight: 5,
+        fontSize: 20,
+        color: "#ffffff",
+    },
+    btn: {
+        backgroundColor: "#ff0000",
+        marginRight: 10,
+        borderRadius: 10,
+    },
     main: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        flexDirection: "row",
+        flexWrap: "wrap",
     },
 });
 
