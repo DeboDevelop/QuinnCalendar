c5857d20fdae3b992720fcb8a634a5a794b58f2f
DeboDevelop <debajyotidofficial@gmail.com>
2021-01-27 20:07:40 +0530

    Created the Calendar.

--- components/pages/Calendar.js
+++ components/pages/Calendar.js
@@ -23,18 +23,19 @@ export class Calendar extends Component {
         super(props);
         this.state = {
             mounted: false,
-            calender: Array(49).fill(0),
+            calender: [],
             day: new Date().getDate(),
             month: new Date().getMonth(),
             year: new Date().getFullYear(),
         };
         this.forceUpdateHandler = this.forceUpdateHandler.bind(this);
+        this.getCalender = this.getCalender.bind(this);
         Dimensions.addEventListener("change", () => {
             this.forceUpdateHandler();
         });
     }
     componentDidMount() {
-        this.setState({ ...this.state, mounted: true });
+        this.setState({ ...this.state, mounted: true, calender: this.getCalender(this.state.month, this.state.year) });
     }
     componentWillUnmount() {
         this.setState({ ...this.state, mounted: false });
@@ -46,34 +47,88 @@ export class Calendar extends Component {
             }, 500);
         }
     }
+    getCalender(month, year) {
+        let result = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+        let daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+        if ((year % 4 === 0 && year % 100 === 0) || year % 400 === 0) {
+            daysInMonth[1] = 29;
+        }
+        const blanks = new Date(year, month, 1).getDay();
+        for (let i = 0; i < blanks; i++) {
+            result.push("");
+        }
+        for (let i = 1; i <= daysInMonth[month]; i++) {
+            result.push(i);
+        }
+        for (let i = 0; i < 42 - blanks - daysInMonth[month]; i++) {
+            result.push("");
+        }
+        return result;
+    }
     onSwipeUp(gestureState) {
         if (this.state.month === 0) {
-            this.setState({ ...this.state, month: 11, year: this.state.year - 1 });
+            this.setState({
+                ...this.state,
+                month: 11,
+                year: this.state.year - 1,
+                calender: this.getCalender(11, this.state.year - 1),
+            });
         } else {
-            this.setState({ ...this.state, month: this.state.month - 1 });
+            this.setState({
+                ...this.state,
+                month: this.state.month - 1,
+                calender: this.getCalender(this.state.month - 1, this.state.year),
+            });
         }
     }
 
     onSwipeDown(gestureState) {
         if (this.state.month === 11) {
-            this.setState({ ...this.state, month: 0, year: this.state.year + 1 });
+            this.setState({
+                ...this.state,
+                month: 0,
+                year: this.state.year + 1,
+                calender: this.getCalender(0, this.state.year + 1),
+            });
         } else {
-            this.setState({ ...this.state, month: this.state.month + 1 });
+            this.setState({
+                ...this.state,
+                month: this.state.month + 1,
+                calender: this.getCalender(this.state.month + 1, this.state.year),
+            });
         }
     }
     onSwipeLeft(gestureState) {
         if (this.state.month === 0) {
-            this.setState({ ...this.state, month: 11, year: this.state.year - 1 });
+            this.setState({
+                ...this.state,
+                month: 11,
+                year: this.state.year - 1,
+                calender: this.getCalender(11, this.state.year - 1),
+            });
         } else {
-            this.setState({ ...this.state, month: this.state.month - 1 });
+            this.setState({
+                ...this.state,
+                month: this.state.month - 1,
+                calender: this.getCalender(this.state.month - 1, this.state.year),
+            });
         }
     }
 
     onSwipeRight(gestureState) {
         if (this.state.month === 11) {
-            this.setState({ ...this.state, month: 0, year: this.state.year + 1 });
+            this.setState({
+                ...this.state,
+                month: 0,
+                year: this.state.year + 1,
+                calender: this.getCalender(0, this.state.year + 1),
+            });
         } else {
-            this.setState({ ...this.state, month: this.state.month + 1 });
+            this.setState({
+                ...this.state,
+                month: this.state.month + 1,
+                calender: this.getCalender(this.state.month + 1, this.state.year),
+            });
         }
     }
     render() {
